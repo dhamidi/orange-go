6b076d200dec689560de5c8531c26bf7883272bb
Dario Hamidi <dario.hamidi@gmail.com>
2024-07-26 11:37:09 +0300

    ops: add build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+case "${1:-local}" in
+"digitalocean")
+  export CGO_ENABLED=1
+  export GOARCH=amd64
+  export GOOS=linux
+  export CC=x86_64-linux-musl-gcc
+  export CXX=x86_64-linux-musl-g++
+  go build -ldflags "-linkmode external -extldflags -static" -o ./orange-linux .
+  ;;
+"local")
+  go build .
+  ;;
+esac
