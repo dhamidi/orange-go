b5729ef2d20bf98855d60a74cd08458c339aa29c
Dario Hamidi <dario.hamidi@gmail.com>
2024-07-28 10:09:08 +0300

    chore: formatting

--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@
 export GOEXPERIMENT=rangefunc
 
 case "${1:-local}" in
-"digitalocean")
-  export CGO_ENABLED=1
-  export GOARCH=amd64
-  export GOOS=linux
-  export CC=x86_64-linux-musl-gcc
-  export CXX=x86_64-linux-musl-g++
-  go build -ldflags "-linkmode external -extldflags -static" -o ./orange-linux .
+  "digitalocean")
+    export CGO_ENABLED=1
+    export GOARCH=amd64
+    export GOOS=linux
+    export CC=x86_64-linux-musl-gcc
+    export CXX=x86_64-linux-musl-g++
+    go build -ldflags "-linkmode external -extldflags -static" -o ./orange-linux .
   ;;
-"local")
-  go build .
+  "local")
+    go build .
   ;;
 esac
