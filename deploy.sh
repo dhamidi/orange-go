5deee09a149457158433a96b1ebc9a4979a25dd9
Dario Hamidi <dario.hamidi@gmail.com>
2024-07-29 12:56:06 +0300

    dev: make deploy script more robust
    On the server:
    
    mv orange.next orange
    orange serve 127.0.0.1:8081

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ deploy_path=$(git config deploy.path)
 case "${1:-push}" in
   push)
     ./build.sh digitalocean
-    scp ./orange-linux "$deploy_user"@"$deploy_host":"$deploy_path"
+    scp ./orange-linux "$deploy_user"@"$deploy_host":"$deploy_path.next"
     ;;
   shell)
     exec ssh "$deploy_user"@"$deploy_host"
