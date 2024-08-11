84f33d8ac8a9555e9a06930cf4cf971d1f1f9359
VladPodilnyk <vladyslav.podilnyk@bolt.eu>
2024-08-11 14:14:54 +0300

    wip: link genertor process scaffold.

--- magic_login.go
+++ magic_login.go
@@ -1,6 +1,9 @@
 package main
 
-import "log"
+import (
+	"log"
+	"time"
+)
 
 type MagicLoginController struct {
 	Logger   *log.Logger
@@ -26,9 +29,38 @@ func (m *MagicLoginController) HandleCommand(command Command) error {
 }
 
 func (m *MagicLoginController) Start() func() {
-	panic("implement me")
+	stop := make(chan struct{})
+
+	commands, err := m.Commands.After(0)
+	if err != nil {
+		m.Logger.Printf("failed to fetch commands: %v", err)
+		return func() {}
+	}
+
+	// TODO: weird place
+	for c := range commands {
+		m.HandleCommand(c.Message)
+	}
+
+	go m.loop(stop)
+	return func() { close(stop) }
 }
 
 func (m *MagicLoginController) handleMagicLogin(c *RequestMagicLinkLogin) error {
 	panic("implement me")
+}
+
+func (p *MagicLoginController) loop(stop <-chan struct{}) {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+	tick := ticker.C
+	for {
+		select {
+		case <-stop:
+			return
+		case <-tick:
+			// p.fetchPreviews()
+			panic("implement me")
+		}
+	}
 }
