7b1c665b05a662b93b795995aa19b9429b3bb4d1
VladPodilnyk <vladyslav.podilnyk@bolt.eu>
2024-08-13 17:15:23 +0300

    magic login controller.

--- magic_login.go
+++ magic_login.go
@@ -1,40 +1,37 @@
 package main
 
 import (
-	"iter"
+	"fmt"
 	"log"
 	"time"
+
+	"github.com/google/uuid"
 )
 
-type RequestState struct {
-	Magic       string
-	IsCompleted bool
-	RetriesLeft uint
-}
-
 type MagicLoginController struct {
+	BaseUrl         string
 	Logger          *log.Logger
 	App             *App
 	Commands        CommandLog
-	MailClient      MailAgent
-	NumberOfRetries uint
-	PendingRequests map[string]RequestState
+	PendingRequests map[string]string
+	Version         int
 }
 
-func NewMagicLoginController(app *App, commands CommandLog, logger *log.Logger, retries uint) *MagicLoginController {
+func NewMagicLoginController(app *App, commands CommandLog, logger *log.Logger, baseUrl string) *MagicLoginController {
 	return &MagicLoginController{
+		BaseUrl:         baseUrl,
 		Logger:          logger,
 		App:             app,
 		Commands:        commands,
-		NumberOfRetries: retries,
-		PendingRequests: make(map[string]RequestState),
+		PendingRequests: make(map[string]string),
+		Version:         0,
 	}
 }
 
-func (m *MagicLoginController) HandleCommand(command Command) error {
+func (m *MagicLoginController) HandleCommand(command Command, from time.Time) error {
 	switch c := command.(type) {
 	case *RequestMagicLinkLogin:
-		return m.addPendingRequest(c)
+		return m.addPendingRequest(c, from)
 	default:
 		return ErrCommandNotAccepted
 	}
@@ -42,26 +39,26 @@ func (m *MagicLoginController) HandleCommand(command Command) error {
 
 func (m *MagicLoginController) Start() func() {
 	stop := make(chan struct{})
-
-	commands, err := m.Commands.After(0)
-	if err != nil {
-		m.Logger.Printf("failed to fetch commands: %v", err)
-		return func() {}
-	}
-
-	then := time.Now().Add(time.Duration(-1) * time.Hour)
-	// Merge latest requests with pending request ??
-	latestRequests := getRequestsUntil(commands, then)
-
-	for c := range latestRequests {
-		m.HandleCommand(c.Message)
-	}
-
+	m.catchUp()
 	go m.loop(stop)
 	return func() { close(stop) }
 }
 
-func (p *MagicLoginController) loop(stop <-chan struct{}) {
+func (m *MagicLoginController) catchUp() {
+	commands, err := m.Commands.After(m.Version)
+	if err != nil {
+		m.Logger.Printf("failed to fetch commands: %v", err)
+		return
+	}
+
+	from := time.Now().Add(time.Duration(-1) * time.Hour)
+	for c := range commands {
+		m.HandleCommand(c.Message, from)
+		m.Version = c.ID
+	}
+}
+
+func (m *MagicLoginController) loop(stop <-chan struct{}) {
 	ticker := time.NewTicker(5 * time.Second)
 	defer ticker.Stop()
 	tick := ticker.C
@@ -70,27 +67,43 @@ func (p *MagicLoginController) loop(stop <-chan struct{}) {
 		case <-stop:
 			return
 		case <-tick:
-			p.handleMagicLogin()
+			m.catchUp()
+			m.handleMagicLogin()
 		}
 	}
 }
 
-func (m *MagicLoginController) addPendingRequest(c *RequestMagicLinkLogin) error {
-	m.PendingRequests[c.Email] = RequestState{
-		Magic:       c.Magic,
-		IsCompleted: false,
-		RetriesLeft: m.NumberOfRetries,
+func (m *MagicLoginController) addPendingRequest(c *RequestMagicLinkLogin, from time.Time) error {
+	if c.RequestedAt.After(from) {
+		m.PendingRequests[c.Email] = c.Magic
 	}
 	return nil
 }
 
 func (m *MagicLoginController) handleMagicLogin() error {
-	// for email, requestState := range m.PendingRequests {
+	toBeRemoved := []string{}
 
-	// }
+	for email, magic := range m.PendingRequests {
+		query := NewFindUserByEmail(email)
+		if err := m.App.HandleQuery(query); err == nil && query.User != nil {
+			queueEmail := QueueEmail{
+				InternalID:   uuid.NewString(),
+				Recipients:   email,
+				Subject:      "",
+				TemplateName: "password-reset",
+				TemplateData: map[string]any{
+					"name":       query.User.Username,
+					"action_url": fmt.Sprintf("%s/login/%s", m.BaseUrl, magic),
+				},
+			}
+			m.App.HandleCommand(&queueEmail)
+		}
+		toBeRemoved = append(toBeRemoved, email)
+	}
+
+	for _, email := range toBeRemoved {
+		delete(m.PendingRequests, email)
+	}
+
 	return nil
-}
-
-func getRequestsUntil(commands iter.Seq[*PersistedCommand], until time.Time) iter.Seq[*PersistedCommand] {
-	panic("implement me")
 }
