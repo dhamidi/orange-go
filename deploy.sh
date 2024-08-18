6db7514b84d14f4b68ac51529a3eb94cb93dcd65
Dario Hamidi <dario.hamidi@gmail.com>
2024-08-18 13:19:14 +0300

    ops: include timestamp in filename
    This avoids conflicts on the server when the executable is running already

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,11 @@ set -eux
 deploy_user=$(git config deploy.user)
 deploy_host=$(git config deploy.host)
 deploy_path=$(git config deploy.path)
+version=$(date +%Y%m%d%H%M%S)
 case "${1:-push}" in
   push)
     ./build.sh digitalocean
-    scp ./orange-linux "$deploy_user"@"$deploy_host":"$deploy_path.next"
+    scp ./orange-linux "$deploy_user"@"$deploy_host":"$deploy_path.$version"
     ;;
   shell)
     exec ssh "$deploy_user"@"$deploy_host"
