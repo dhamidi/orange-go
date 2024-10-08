987184f8237822f75c59ec301f4ffc226c4394b5
Dario Hamidi <dario.hamidi@gmail.com>
2024-10-08 15:03:36 +0300

    test: add session-based web test

--- web_login_test.go
+++ web_login_test.go
@@ -7,3 +7,12 @@ func TestWebApp_PageLogin_hasLogin_Form(t *testing.T) {
 	res := w.send("GET", "/login")
 	res.expectFormFields(Field("username", "text"), Field("password", "password"))
 }
+
+func TestWebApp_PageItem_displays_submissions(t *testing.T) {
+	w := NewWebTest(t)
+	w.RegisterUser("guest")
+	session := w.LogInAs("guest")
+	session.SubmitItem("https://example.com", "Example")
+	session.ExpectPath("/")
+	session.GoToSubmission(0)
+}
--- web_test.go
+++ web_test.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"log"
+	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
+	"time"
 
 	"golang.org/x/net/html"
 )
+
+type RequestOption interface {
+	BuildRequest(req *http.Request)
+}
+
+type RequestOptionFunc func(req *http.Request)
+
+func (f RequestOptionFunc) BuildRequest(req *http.Request) { f(req) }
+
+func SetCookie(name, value string) RequestOption {
+	return RequestOptionFunc(func(req *http.Request) {
+		req.AddCookie(&http.Cookie{Name: name, Value: value})
+	})
+}
 
 type WebTest struct {
 	web  *WebApp
@@ -16,15 +34,49 @@ type WebTest struct {
 }
 
 func (w *WebTest) send(method, path string) *Response {
+	w.test.Logf("-> %s %s", method, path)
 	req := httptest.NewRequest(method, path, nil)
 	res := httptest.NewRecorder()
 	w.web.ServeHTTP(res, req)
+	w.test.Logf("<- %s %s %d", method, path, res.Result().StatusCode)
+	return &Response{raw: res, test: w.test}
+}
+
+func (w *WebTest) post(path string, body url.Values, options ...RequestOption) *Response {
+	w.test.Logf("-> POST %s %s", path, body)
+	req := httptest.NewRequest("POST", path, bytes.NewBufferString(body.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	for _, opt := range options {
+		opt.BuildRequest(req)
+	}
+	res := httptest.NewRecorder()
+	w.web.ServeHTTP(res, req)
+	w.test.Logf("<- POST %s %d %s", path, res.Result().StatusCode, res.Result().Header.Get("Location"))
 	return &Response{raw: res, test: w.test}
 }
 
 type Response struct {
 	raw  *httptest.ResponseRecorder
 	test *testing.T
+}
+
+func (res *Response) Location() *url.URL {
+	loc, err := res.raw.Result().Location()
+	if err != nil {
+		if errors.Is(err, http.ErrNoLocation) {
+			return nil
+		}
+		res.test.Fatalf("Failed to parse location: %s", err)
+	}
+	return loc
+}
+
+func (res *Response) Find(tag string, attrs ...string) *html.Node {
+	doc, err := html.Parse(res.raw.Body)
+	if err != nil {
+		res.test.Fatalf("Error parsing response body as HTML: %v", err)
+	}
+	return findNode(doc, tag, attrs...)
 }
 
 func (res *Response) expectFormFields(fields ...*FormField) {
@@ -90,4 +142,69 @@ func NewWebTest(t *testing.T) *WebTest {
 	logs := bytes.NewBuffer(nil)
 	web.logger = log.New(logs, "[web]", log.LstdFlags)
 	return &WebTest{web: web, logs: logs, test: t}
+}
+
+type WebSession struct {
+	test      *WebTest
+	sessionID string
+	location  *url.URL
+}
+
+func (s *WebSession) ExpectPath(p string) {
+	if s.location.Path != p {
+		s.test.test.Fatalf("Expected path %q, current location is %q", p, s.location.Path)
+	}
+}
+
+func (s *WebSession) SubmitItem(u, title string) {
+	response := s.test.post("/submit", url.Values{
+		"title": []string{title},
+		"url":   []string{u},
+	}, SetCookie("session_id", s.sessionID))
+	s.location = response.Location()
+}
+
+func (w *WebTest) RegisterUser(username string) {
+	password, err := HashPassword("password")
+	if err != nil {
+		w.test.Fatalf("failed to hash password for %s: %s", username, err)
+	}
+	err = w.web.app.HandleCommand(&SignUpUser{
+		PasswordHash: *password,
+		Username:     username,
+		CreatedAt:    time.Now(),
+	})
+	if err != nil {
+		w.test.Fatalf("failed to sign up user %s: %s", username, err)
+	}
+}
+
+func (w *WebTest) LogInAs(username string) *WebSession {
+	response := w.post("/login", url.Values{"username": []string{username}, "password": []string{"password"}})
+	var sessionID *string = nil
+	for _, cookie := range response.raw.Result().Cookies() {
+		if cookie.Name == "session_id" {
+			sessionID = &cookie.Value
+			break
+		}
+	}
+	if sessionID == nil {
+		w.test.Fatalf("No sessionID cookie returned: %s", response.raw.Body.String())
+	}
+	return &WebSession{test: w, sessionID: *sessionID, location: response.Location()}
+}
+
+func (s *WebSession) GoToSubmission(index int) {
+	q := NewFrontpageQuery(nil)
+	if err := s.test.web.app.HandleQuery(q); err != nil {
+		s.test.test.Fatalf("Failed to query submissions: %s", err)
+	}
+	if len(q.Submissions) < index {
+		s.test.test.Fatalf("Index out of range (%d submissions found, looking for %d)", len(q.Submissions), index)
+	}
+	itemURL := &url.URL{Path: "/item", RawQuery: "id=" + q.Submissions[index].ItemID}
+	response := s.test.send("GET", itemURL.String())
+	if loc := response.Location(); loc != nil {
+		s.location = response.Location()
+	}
 }
